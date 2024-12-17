923eab4676e00fc09dfcb9815de43946f56f4028
Kushagra Kulshrestha <140870916+Ricky30feb@users.noreply.github.com>
2024-12-17 16:20:07 +0530

    Initial Commit [Project Created]

---
+++ SpeechMaster/SpeechMasterTests/SpeechMasterTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechMasterTests.swift
+//  SpeechMasterTests
+//
+//  Created by Batch - 2 on 17/12/24.
+//
+
+import Testing
+@testable import SpeechMaster
+
+struct SpeechMasterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
