486fa823be35a78f488da6f7c0bd14afc6df0e94
Arnav-420 <150315484+Arnav-420@users.noreply.github.com>
2025-01-20 11:21:18 +0530

    [Developed]ProgressView Xib File Added

---
+++ SpeechMaster/SpeechMaster/Arnav-Work/CircleProgressView/circularProgressBar.swift
@@ -0,0 +1,52 @@
+//
+//  circularProgressBar.swift
+//  app1
+//
+//  Created by Arnav Chauhan on 13/01/25.
+//
+
+import UIKit
+
+class circularProgressBar: UIView {
+    
+
+        // Properties
+        var progress: CGFloat = 0.5 { // Value between 0 and 1
+            didSet {
+                setNeedsDisplay() // Redraw when progress changes
+            }
+        }
+        var progressColor: UIColor = .systemBlue
+        var trackColor: UIColor = .white
+        var lineWidth: CGFloat = 10.0
+
+        // Drawing the progress bar
+        override func draw(_ rect: CGRect) {
+            let center = CGPoint(x: rect.width / 2, y: rect.height / 2)
+            let radius = min(rect.width, rect.height) / 2 - lineWidth / 2
+
+            // Draw the track (background circle)
+            let trackPath = UIBezierPath(
+                arcCenter: center,
+                radius: radius,
+                startAngle: -CGFloat.pi / 2,
+                endAngle: CGFloat.pi * 2 - CGFloat.pi / 2,
+                clockwise: true
+            )
+            trackPath.lineWidth = lineWidth
+            trackColor.setStroke()
+            trackPath.stroke()
+
+            // Draw the progress (foreground circle)
+            let progressPath = UIBezierPath(
+                arcCenter: center,
+                radius: radius,
+                startAngle: -CGFloat.pi / 2,
+                endAngle: (-CGFloat.pi / 2) + (progress * 2 * CGFloat.pi),
+                clockwise: true
+            )
+            progressPath.lineWidth = lineWidth
+            progressColor.setStroke()
+            progressPath.stroke()
+        }
+    }
---
+++ SpeechMaster/SpeechMaster/Arnav-Work/ColllectionViewComponents/ProgressCollectionViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  ProgressCollectionViewCell.swift
+//  app1
+//
+//  Created by Arnav Chauhan on 14/01/25.
+//
+
+import UIKit
+
+class ProgressCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var title1: UILabel!
+    @IBOutlet weak var title1Percent: UILabel!
+    
+    @IBOutlet weak var percent1: circularProgressBar!
+    
+    @IBOutlet weak var topicName1: UILabel!
+    
+    @IBOutlet weak var title2: UILabel!
+    
+    @IBOutlet weak var title2percent: UILabel!
+    
+    @IBOutlet weak var topicName2: UILabel!
+    @IBOutlet weak var percent2: circularProgressBar!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    @IBOutlet weak var view1: UIView!
+    
+    @IBOutlet weak var view2: UIView!
+    
+    
+    
+    
+    
+    
+    
+    func updateCircle1(percentage: Double, progresscolor: UIColor) {
+        guard let circularProgressBar = percent1 else { return }
+        circularProgressBar.progress = percentage
+        circularProgressBar.lineWidth = 12
+        circularProgressBar.progressColor = progresscolor
+        circularProgressBar.trackColor = .white
+        
+    
+    }
+    func updateCircle2(percentage: Double, progresscolor: UIColor) {
+        guard let circularProgressBar = percent2 else { return }
+        circularProgressBar.progress = percentage
+        circularProgressBar.lineWidth = 12
+        circularProgressBar.progressColor = progresscolor
+        circularProgressBar.trackColor = .white
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        updateRound()
+        // Initialization code
+    }
+    func updateRound(){
+        view1.layer.cornerRadius = 15
+        view2.layer.cornerRadius = 15
+        image.layer.cornerRadius = 15
+        image.clipsToBounds = true
+    }
+
+}
