6ae66e14d0c7cd7cb830f7533963143c771412ed
Kushagra Kulshrestha <140870916+Ricky30feb@users.noreply.github.com>
2025-02-26 22:57:51 +0530

    [Fix]: Fixed Rehearse Again

--- SpeechMaster/SpeechMaster/Arnav-Work/Progress Session/ScriptEditViewController.swift
+++ SpeechMaster/SpeechMaster/Arnav-Work/Progress Session/ScriptEditViewController.swift
@@ -18,16 +18,5 @@ class ScriptEditViewController: UIViewController {
         //HomeViewModel.shared.
     }
 
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
